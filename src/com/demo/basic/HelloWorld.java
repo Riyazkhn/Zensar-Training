1f2eaed3af60760b1b032b9a0a8a96edd61ef329
Riyaz khan <riyazkhn44@gmail.com>
2021-09-03 11:10:44 +0530

    Hello world!

---
+++ src/com/demo/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.demo.basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int numberOne = 100;
+		int numberTwo = 400;
+		int result = numberOne + numberTwo;
+
+		System.out.println("Addition:  " + result);
+
+	}
+
+}
