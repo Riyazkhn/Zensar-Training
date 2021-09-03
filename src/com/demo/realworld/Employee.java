30973e57822c9c930226080cd56c1b4618906f61
Riyaz khan <riyazkhn44@gmail.com>
2021-09-03 11:23:34 +0530

    Encapsulations

---
+++ src/com/demo/realworld/Employee.java
@@ -0,0 +1,31 @@
+package com.demo.realworld;
+
+public class Employee {
+	private int salary, id;
+	private String name;
+
+	public void setId(int x) {
+		id = x;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setSalary(int x) {
+		salary = x;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setName(String x) {
+		name = x;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ src/com/demo/realworld/Encapsulation.java
@@ -0,0 +1,16 @@
+package com.demo.realworld;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.setId(1);
+		e.setSalary(5000);
+		e.setName("Sohil");
+		System.out.println(e.getId());
+		System.out.println(e.getSalary());
+		System.out.println(e.getName());
+
+	}
+
+}
